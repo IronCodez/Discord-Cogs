df7ab8adb2a3da9b4ed2d9c7d3020b8234951999
Iron <IronCodez@gmail.com>
2020-06-02 23:41:16 -0400

    Update cogexample.py

--- cogs/cogexample.py
+++ cogs/cogexample.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
 
+class examplecog(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    #Example of an event in a cog.
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+      print("This is an example of an event in a cog!")
+      
+    #Example of a command in a cog.
+    @commands.command()
+    async def command(self, ctx):
+        await ctx.send("This is an example of a command in a cog!")
+      
+def setup(bot):
+  bot.add_cog(fun(bot))
