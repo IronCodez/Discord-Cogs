63a108b8ea03a4bac40bf0c98dc722439538e0fa
Iron <IronCodez@gmail.com>
2020-06-02 23:43:09 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import discord
+from discord.ext import commands
 
+client = commands.Bot(command_prefix = "prefix")
+token = "token"
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+        
+client.run(token)
