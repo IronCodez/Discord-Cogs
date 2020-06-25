49607c330877d2c8d6ca287454e3123f7fa04e57
Iron <IronCodez@gmail.com>
2020-06-25 00:34:55 -0400

    Update admincog.py

--- cogs/admincog.py
+++ cogs/admincog.py
@@ -8,20 +8,20 @@ class Admin(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def load(self, ctx, extension):
-        client.load_extension(f'cogs.{extension}')
+        self.client.load_extension(f'cogs.{extension}')
         await ctx.send(f":white_check_mark: Successfully loaded the cog `{extension}`")
 
     @commands.command()
     @commands.is_owner()
     async def unload(self, ctx, extension):
-        client.unload_extension(f'cogs.{extension}')
+        self.client.unload_extension(f'cogs.{extension}')
         await ctx.send(f":white_check_mark: Successfully unloaded the cog `{extension}`")
 
     @commands.command()
     @commands.is_owner()
     async def reload(self, ctx, extension):
-        client.unload_extension(f'cogs.{extension}')
-        client.load_extension(f'cogs.{extension}')
+        self.client.unload_extension(f'cogs.{extension}')
+        self.client.load_extension(f'cogs.{extension}')
         await ctx.send(f":white_check_mark: Successfully reloaded the cog `{extension}`")
     
 def setup(bot):
