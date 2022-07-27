fb9c57a075059463812fea3c6f26c9174075c2ca
Zen <IronCodez@gmail.com>
2022-07-26 22:45:16 -0400

    Updated to 2.0, look at histrory for 1.0

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
-import discord, os
+import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = "prefix")
-token = os.environ['TOKEN']
+client = discord.Client(command_prefix=None, intents=None)
+TOKEN = "your_token_here"
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-        
-client.run(token)
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
+            print(f"Loaded cog.{filename[:-3]}")
+
+async def main():
+    async with client:
+        await load_extensions()
+        await client.start(TOKEN))
